bd6e367cf7864ab2fe076cd03d9fe1345b454bd0
Joshua <jpe7619@gmail.com>
2023-11-16 16:10:47 -0600

    update photogrid alignment

--- src/WelcomePage/index.js
+++ src/WelcomePage/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import './styles.css'
 
 const WelcomePage = () => {
-  const str = "Welcome to my Photography Portfolio";
+  const str = "@strykerhype";
   return(
     <>
     <div className="welcome-splash">
