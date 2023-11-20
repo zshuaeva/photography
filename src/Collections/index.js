81be854e87c67fbbcb48e333b88f5656ccd52ad6
Joshua <jpe7619@gmail.com>
2023-11-20 13:46:34 -0600

    update items in photogrid  to true center with dynamic page sizing, update page colors

--- src/Collections/index.js
+++ src/Collections/index.js
@@ -7,25 +7,25 @@ function Collections() {
   const cld = new Cloudinary({ cloud: { cloudName: 'dpp71t8ww' } });
   console.log(cld);
 
-  // Assuming you have a route path for the PhotoGrid component
+
   const photoGridPath = "/photogrid";
 
   return (
     <>
-      <Link to={photoGridPath} className="flex-container">
-        <div className="box photo">
+      <Link to={photoGridPath} className="collection-flex-container">
+        <div className="collection-box collection-photo">
           {/* load first 6 images in collections here */}
           <img
-            className="img"
+            className="collection-img"
             alt="collection stack"
             src='https://res.cloudinary.com/dpp71t8ww/image/upload/v1698442467/sakhir_heodkr.pn'
           />
         </div>
-        <div className="wrapper">
-          <div className="box description"> Description:
+        <div className="collection-wrapper">
+          <div className="collection-box collection-description"> Description:
             <p>Collection Description</p>
           </div>
-          <div className="box date">Date Updated:
+          <div className="collection-box collection-date">Date Updated:
             {/* pass in date of last upload here */}
             <p>01-01-2023</p>
           </div>
